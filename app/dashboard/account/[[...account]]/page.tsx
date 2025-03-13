6524891c380f0b5d7cbd6ed7aede2f9df91da251
yzy98 <yz186337@163.com>
2025-03-13 22:30:28 +0800

    customize dashboard account page

--- app/dashboard/account/[[...account]]/page.tsx
+++ app/dashboard/account/[[...account]]/page.tsx
@@ -1,6 +1,5 @@
 import { AccountInfoCard } from "@/components/dashboard/account/info-card";
 
-// TODO: Re-create the account page
 export default function AccountPage() {
   return <AccountInfoCard />;
 }
--- components/dashboard/account/info-card.tsx
+++ components/dashboard/account/info-card.tsx
@@ -1,3 +1,17 @@
 import { UserProfile } from "@clerk/nextjs";
 
-export const AccountInfoCard = () => <UserProfile />;
+export const AccountInfoCard = () => (
+  <UserProfile
+    path="/dashboard/account"
+    routing="path"
+    appearance={{
+      elements: {
+        navbar: "hidden!",
+        navbarMobileMenuRow: "hidden!",
+        cardBox: "rounded-lg! border! shadow-sm!",
+        scrollBox: "bg-card!",
+        actionCard: "bg-card!",
+      },
+    }}
+  />
+);
